574e5e85362d8996fd873ecd496d39b94de3538b
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-09-29 10:37:19 +0700

    miner.py

--- miner.py
+++ miner.py
@@ -1,19 +1,96 @@
-import json
-import requests
-import time
+import json, requests, time, hashlib, string, threading, re, configparser, os
 from passlib.hash import argon2
-import hashlib
 from random import choice, randrange
-import string
-import threading
-import re
 
-difficulty = 1
-memory_cost = 1500 
-cores = 1
-account = "0x8e0eF38146634d387A7302d084bDCdC000999999"
-print(account,difficulty,memory_cost,cores)
-print('------XUNI------')
+import argparse
+import configparser
+
+
+# Set up argument parser
+parser = argparse.ArgumentParser(description="Process optional account and worker arguments.")
+parser.add_argument('--account', type=str, help='The account value to use.')
+parser.add_argument('--worker', type=int, help='The worker id to use.')
+parser.add_argument('--gpu', type=str, help='Set to true to enable GPU mode, and to false to disable it.')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Access the arguments via args object
+account = args.account
+worker_id = args.worker
+gpu_mode = args.gpu
+
+# For example, to print the values
+print(f'Account: {account}, Worker ID: {worker_id}, GPU Mode: {gpu_mode}')
+
+# Load the configuration file
+config = configparser.ConfigParser()
+config_file_path = '/content/XENGPUMiner/config.conf'
+
+if os.path.exists(config_file_path):
+    config.read(config_file_path)
+else:
+    raise FileNotFoundError(f"The configuration file {config_file_path} was not found.")
+
+# Override account from config file with command line argument if provided
+if args.account:
+    account = args.account
+else:
+    # Ensure that the required settings are present
+    required_settings = ['difficulty', 'memory_cost', 'cores', 'account', 'last_block_url']
+    if not all(key in config['Settings'] for key in required_settings):
+        missing_keys = [key for key in required_settings if key not in config['Settings']]
+        raise KeyError(f"Missing required settings: {', '.join(missing_keys)}")
+
+    account = config['Settings']['account']
+
+if args.gpu is not None:
+    if args.gpu.lower() == 'true':
+        gpu_mode = True
+    else:
+        gpu_mode = False
+else:
+    # Ensure that the required settings are present
+    if 'gpu_mode' not in config['Settings']:
+        missing_keys = [key for key in required_settings if key not in config['Settings']]
+        print(f"Missing gpu_mode settings, defaulting to False")
+        gpu_mode = False
+    else:
+        if config['Settings']['gpu_mode'].lower() == 'true':
+            gpu_mode = True
+        else:
+            gpu_mode = False
+
+# Access other settings
+difficulty = int(config['Settings']['difficulty'])
+memory_cost = int(config['Settings']['memory_cost'])
+cores = int(config['Settings']['cores'])
+last_block_url = config['Settings']['last_block_url']
+
+
+def hash_value(value):
+    return hashlib.sha256(value.encode()).hexdigest()
+
+def build_merkle_tree(elements, merkle_tree={}):
+    if len(elements) == 1:
+        return elements[0], merkle_tree
+
+    new_elements = []
+    for i in range(0, len(elements), 2):
+        left = elements[i]
+        right = elements[i + 1] if i + 1 < len(elements) else left
+        combined = left + right
+        new_hash = hash_value(combined)
+        merkle_tree[new_hash] = {'left': left, 'right': right}
+        new_elements.append(new_hash)
+    return build_merkle_tree(new_elements, merkle_tree)
+
+from datetime import datetime
+def is_within_five_minutes_of_hour():
+    timestamp = datetime.now()
+    minutes = timestamp.minute
+    return 0 <= minutes < 5 or 55 <= minutes < 60
+
 class Block:
     def __init__(self, index, prev_hash, data, valid_hash, random_data, attempts):
         self.index = index
@@ -44,25 +121,33 @@ class Block:
 
 updated_memory_cost = 1500 # just initialize it
 
+def write_difficulty_to_file(difficulty, filename='difficulty.txt'):
+    with open(filename, 'w') as file:
+        file.write(difficulty)
+
 def update_memory_cost_periodically():
     global memory_cost
     global updated_memory_cost
+    global gpu_mode
     time.sleep(10)  # start checking in 10 seconds after launch 
     while True:
         updated_memory_cost = fetch_difficulty_from_server()
         if updated_memory_cost != memory_cost:
+            if gpu_mode:
+                write_difficulty_to_file(updated_memory_cost)
             print(f"Updating difficulty to {updated_memory_cost}")
         time.sleep(60)  # Fetch every 60 seconds
 
 # Function to get difficulty level from the server
 def fetch_difficulty_from_server():
+    global memory_cost
     try:
         response = requests.get('http://xenminer.mooo.com/difficulty')
         response_data = response.json()
         return str(response_data['difficulty'])
     except Exception as e:
         print(f"An error occurred while fetching difficulty: {e}")
-        return '2000'  # Default value if fetching fails
+        return memory_cost  # Return last value if fetching fails
 
 def generate_random_sha256(max_length=128):
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -74,6 +159,70 @@ def generate_random_sha256(max_length=128):
 
 from tqdm import tqdm
 import time
+
+def submit_pow(account_address, key, hash_to_verify):
+    # Download last block record
+    url = last_block_url
+
+    try:
+        # Attempt to download the last block record
+        response = requests.get(url, timeout=10)  # Adding a timeout of 10 seconds
+    except requests.exceptions.RequestException as e:
+        # Handle any exceptions that occur during the request
+        print(f"An error occurred: {e}")
+        return None  # Optionally return an error value or re-raise the exception
+
+    if response.status_code != 200:
+        # Handle unexpected HTTP status codes
+        print(f"Unexpected status code {response.status_code}: {response.text}")
+        return None  # Optionally return an error value
+
+    if response.status_code == 200:
+        records = json.loads(response.text)
+        verified_hashes = []
+
+        for record in records:
+            block_id = record.get('block_id')
+            record_hash_to_verify = record.get('hash_to_verify')
+            record_key = record.get('key')
+            account = record.get('account')
+
+            # Verify each record using Argon2
+            if record_key is None or record_hash_to_verify is None:
+                print(f'Skipping record due to None value(s): record_key: {record_key}, record_hash_to_verify: {record_hash_to_verify}')
+                continue  # skip to the next record
+
+            if argon2.verify(record_key, record_hash_to_verify):
+                verified_hashes.append(hash_value(str(block_id) + record_hash_to_verify + record_key + account))
+
+        # If we have any verified hashes, build the Merkle root
+        if verified_hashes:
+            merkle_root, _ = build_merkle_tree(verified_hashes)
+
+            # Calculate block ID for output (using the last record for reference)
+            output_block_id = int(block_id / 100)
+
+            # Prepare payload for PoW
+            payload = {
+                'account_address': account_address,
+                'block_id': output_block_id,
+                'merkle_root': merkle_root,
+                'key': key,
+                'hash_to_verify': hash_to_verify
+            }
+
+            # Send POST request
+            pow_response = requests.post('http://xenminer.mooo.com:4446/send_pow', json=payload)
+
+            if pow_response.status_code == 200:
+                print(f"Proof of Work successful: {pow_response.json()}")
+            else:
+                print(f"Proof of Work failed: {pow_response.json()}")
+
+            print(f"Block ID: {output_block_id}, Merkle Root: {merkle_root}")
+
+    else:
+        print("Failed to fetch the last block.")
 
 # ANSI escape codes
 RED = "\033[31m"
@@ -103,18 +252,25 @@ def mine_block(stored_targets, prev_hash):
                     return
 
             random_data = generate_random_sha256()
-            hashed_data = argon2_hasher.hash(random_data + prev_hash)
+            hashed_data = argon2_hasher.hash(random_data)
+
 
             for target in stored_targets:
                 if target in hashed_data[-87:]:
-                    print(f"\n{RED}Found valid hash for target {target} after {attempts} attempts{RESET}")
-                    capital_count = sum(1 for char in re.sub('[0-9]', '', hashed_data) if char.isupper())
+                # Search for the pattern "XUNI" followed by a digit (0-9)
+                    if re.search("XUNI[0-9]", hashed_data) and is_within_five_minutes_of_hour():
+                        found_valid_hash = True
+                        break
+                    elif target == "XEN11":
+                        found_valid_hash = True
+                        capital_count = sum(1 for char in re.sub('[0-9]', '', hashed_data) if char.isupper())
+                        if capital_count >= 65:
+                            print(f"{RED}Superblock found{RESET}")
+                        break
+                    else:
+                        found_valid_hash = False
+                        break
 
-                    if capital_count >= 65:
-                        print(f"{RED}Superblock found{RESET}")
-
-                    found_valid_hash = True
-                    break
 
             pbar.update(1)
 
@@ -124,17 +280,19 @@ def mine_block(stored_targets, prev_hash):
                 pbar.set_postfix({"Difficulty": f"{YELLOW}{memory_cost}{RESET}"}, refresh=True)
 
             if found_valid_hash:
+                print(f"\n{RED}Found valid hash for target {target} after {attempts} attempts{RESET}")
                 break
 
 
     # Prepare the payload
     payload = {
         "hash_to_verify": hashed_data,
-        "key": random_data + prev_hash,
+        "key": random_data,
         "account": account,
         "attempts": attempts,
-        "hashes_per_second": hashes_per_second
-        }
+        "hashes_per_second": hashes_per_second,
+        "worker": worker_id  # Adding worker information to the payload
+    }
 
     print (payload)
 
@@ -147,6 +305,10 @@ def mine_block(stored_targets, prev_hash):
 
         # Print the HTTP status code
         print("HTTP Status Code:", response.status_code)
+
+        if target == "XEN11" and found_valid_hash and response.status_code == 200:
+            #submit proof of work validation of last sealed block
+            submit_pow(account, random_data, hashed_data)
 
         if response.status_code != 500:  # If status code is not 500, break the loop
             print("Server Response:", response.json())
@@ -165,14 +327,126 @@ def mine_block(stored_targets, prev_hash):
 
     return random_data, hashed_data, attempts, hashes_per_second
 
-def verify_block(block):
-    argon2_hasher = argon2.using(time_cost=difficulty, memory_cost=memory_cost, parallelism=cores)
-    #debug
-    print ("Key: ");
-    print (block['random_data'] + block['prev_hash'])
-    print ("Hash: ");
-    print (block['valid_hash'])
-    return argon2_hasher.verify(block['random_data'] + block['prev_hash'], block['valid_hash'])
+normal_blocks_count = 0
+super_blocks_count = 0
+xuni_blocks_count = 0
+def submit_block(key):
+    global updated_memory_cost  # Make it global so that we can update it
+    found_valid_hash = False
+
+    global normal_blocks_count
+    global super_blocks_count
+    global xuni_blocks_count
+
+    argon2_hasher = argon2.using(time_cost=difficulty, salt=b"XEN10082022XEN", memory_cost=updated_memory_cost, parallelism=cores, hash_len = 64)
+    
+    hashed_data = argon2_hasher.hash(key)
+
+    for target in stored_targets:
+        if target in hashed_data[-87:]:
+        # Search for the pattern "XUNI" followed by a digit (0-9)
+            if re.search("XUNI[0-9]", hashed_data) and is_within_five_minutes_of_hour():
+                found_valid_hash = True
+                break
+            elif target == "XEN11":
+                found_valid_hash = True
+                capital_count = sum(1 for char in re.sub('[0-9]', '', hashed_data) if char.isupper())
+                if capital_count >= 65:
+                    print(f"{RED}Superblock found{RESET}")
+                break
+            else:
+                found_valid_hash = False
+                break
+
+    if found_valid_hash:
+        print(f"\n{RED}Found valid hash for target {target}{RESET}")
+        # Prepare the payload
+        payload = {
+            "hash_to_verify": hashed_data,
+            "key": key,
+            "account": account,
+            "attempts": "130000",
+            "hashes_per_second": "1000",
+            "worker": worker_id  # Adding worker information to the payload
+            }
+
+        print (payload)
+
+        max_retries = 2
+        retries = 0
+
+        while retries <= max_retries:
+            # Make the POST request
+            response = requests.post('http://xenminer.mooo.com/verify', json=payload)
+
+            # Print the HTTP status code
+            print("HTTP Status Code:", response.status_code)
+
+            if found_valid_hash and response.status_code == 200:
+                if "XUNI" in hashed_data:
+                    xuni_blocks_count += 1
+                    break
+                elif "XEN11" in hashed_data:
+                    capital_count = sum(1 for char in re.sub('[0-9]', '', hashed_data) if char.isupper())
+                    if capital_count >= 65:
+                        super_blocks_count += 1
+                    else:
+                        normal_blocks_count += 1
+
+            if target == "XEN11" and found_valid_hash and response.status_code == 200:
+                #submit proof of work validation of last sealed block
+                submit_pow(account, key, hashed_data)
+
+            if response.status_code != 500:  # If status code is not 500, break the loop
+                print("Server Response:", response.json())
+                break
+            
+            retries += 1
+            print(f"Retrying... ({retries}/{max_retries})")
+            time.sleep(10)  # You can adjust the sleep time
+
+
+            # Print the server's response
+            try:
+                print("Server Response:", response.json())
+            except Exception as e:
+                print("An error occurred:", e)
+
+    return key, hashed_data
+
+def monitor_blocks_directory():
+    global normal_blocks_count
+    global super_blocks_count
+    global xuni_blocks_count
+    global memory_cost
+    with tqdm(total=None, dynamic_ncols=True, desc=f"{GREEN}Mining{RESET}", unit=f" {GREEN}Blocks{RESET}") as pbar:
+        while True:
+            XENDIR = f"gpu_found_blocks_tmp/"
+            if not os.path.exists(XENDIR):
+                os.makedirs(XENDIR)
+            for filename in os.listdir(XENDIR):
+                filepath = os.path.join(XENDIR, filename)
+                with open(filepath, 'r') as f:
+                    data = f.read()
+                submit_block(data)
+                pbar.update(1)
+                os.remove(filepath)
+            superblock = f"{RED}super:{normal_blocks_count}{RESET} "
+            block = f"{GREEN}normal:{super_blocks_count}{RESET} "
+            xuni = f"{BLUE}xuni:{xuni_blocks_count}{RESET} "
+            if(super_blocks_count == 0):
+                superblock = ""
+            if(normal_blocks_count == 0):
+                block = ""
+            if(xuni_blocks_count == 0):
+                xuni = ""
+            if super_blocks_count == 0 and normal_blocks_count == 0 and xuni_blocks_count == 0:
+                pbar.set_postfix({"Details": f"Waiting for blocks..."}, refresh=True)
+            else:
+                pbar.set_postfix({"Details": f"{superblock}{block}{xuni}"}, refresh=True)
+
+            time.sleep(0.8)  # Check every 1 seconds
+
 
 if __name__ == "__main__":
     blockchain = []
@@ -186,33 +460,37 @@ if __name__ == "__main__":
 
     genesis_block = Block(0, "0", "Genesis Block", "0", "0", "0")
     blockchain.append(genesis_block.to_dict())
-    print(f"Genesis Block: {genesis_block.hash}")
+    print(f"Mining with: {account}")
+    if(gpu_mode):
+        print(f"Using GPU mode")
+        submit_thread = threading.Thread(target=monitor_blocks_directory)
+        submit_thread.daemon = True  # This makes the thread exit when the main program exits
+        submit_thread.start()
+        try:
+            while True:  # Loop forever
+                time.sleep(10)  # Sleep for 10 seconds
+        except KeyboardInterrupt:
+            print("Main thread is finished")
+    else:
+        print(f"Using CPU mode")
+        i = 1
+        while i <= num_blocks_to_mine:
+            print(f"Mining block {i}...")
+            result = mine_block(stored_targets, blockchain[-1]['hash'])
 
-    i = 1
-    while i <= num_blocks_to_mine:
-        print(f"Mining block {i}...")
-        result = mine_block(stored_targets, blockchain[-1]['hash'])
-
-        if result is None:
-            print(f"{RED}Restarting mining round{RESET}")
+            if result is None:
+                print(f"{RED}Restarting mining round{RESET}")
                 # Skip the increment of `i` and continue the loop
-            continue
-        else:
-            i += 1  
+                continue
+            elif result == 2:
+                result = None
+                continue
+            else:
+                i += 1
 
-    random_data, new_valid_hash, attempts, hashes_per_second = result
-    new_block = Block(i, blockchain[-1]['hash'], f"Block {i} Data", new_valid_hash, random_data, attempts)
-    new_block.to_dict()['hashes_per_second'] = hashes_per_second
-    blockchain.append(new_block.to_dict())
-    print(f"New Block Added: {new_block.hash}")
+        random_data, new_valid_hash, attempts, hashes_per_second = result
+        new_block = Block(i, blockchain[-1]['hash'], f"Block {i} Data", new_valid_hash, random_data, attempts)
+        new_block.to_dict()['hashes_per_second'] = hashes_per_second
+        blockchain.append(new_block.to_dict())
+        print(f"New Block Added: {new_block.hash}")
 
-
-    # Verification
-    for i, block in enumerate(blockchain[1:], 1):
-        is_valid = verify_block(block)
-        print(f"Verification for Block {i}: {is_valid}")
-
-    # Write blockchain to JSON file
-    blockchain_json = json.dumps(blockchain, indent=4)
-    with open("blockchain.json", "w") as f:
-        f.write(blockchain_json)
